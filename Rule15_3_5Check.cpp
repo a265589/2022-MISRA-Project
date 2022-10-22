e7feff19a37b9915d8e24014ba70976938405967
Andy Chen <a265589@yahoo.com.tw>
2022-10-22 17:30:25 +0800

    Rule15-3-5 modified

--- Rule15_3_5Check.cpp
+++ Rule15_3_5Check.cpp
@@ -18,14 +18,24 @@ namespace misra {
 
 void Rule15_3_5Check::registerMatchers(MatchFinder *Finder) {
   // FIXME: Add matchers.
-  Finder->addMatcher(varDecl(allOf(hasParent(cxxCatchStmt()), hasType(cxxRecordDecl(isStruct())))).bind("class_type_exception"), this);
+  //Finder->addMatcher(varDecl(allOf(hasParent(cxxCatchStmt()), hasType(cxxRecordDecl(isStruct())))).bind("class_type_exception"), this);
+  Finder->addMatcher(cxxCatchStmt().bind("catch_stmt"), this);
 }
 
 void Rule15_3_5Check::check(const MatchFinder::MatchResult &Result) {
   // FIXME: Add callback implementation.
-  const auto *Matched = Result.Nodes.getNodeAs<VarDecl>("class_type_exception");
-    diag(Matched->getBeginLoc(), "A class type exception shall always be caught by reference.")
-    << FixItHint::CreateInsertion(Matched->getBeginLoc(),"");
+  const auto *Matched = Result.Nodes.getNodeAs<CXXCatchStmt>("catch_stmt");
+  const auto QualifiedType = Matched->getCaughtType();
+  if(!QualifiedType.isNull())
+  {
+     const auto *TypePtr = QualifiedType.getTypePtr();
+     if( TypePtr->isStructureOrClassType()&&!TypePtr->isReferenceType()  )
+       diag(Matched->getBeginLoc(), "A class type exception shall always be caught by reference.");
+  }
+  
+  
+
+    
 }
 
 } // namespace misra
